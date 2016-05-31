9b3d6390070f17496159eb71c1bf64749843d572
Andres Gomez Urquiza <andres@fractal.ai>
2016-05-30 19:07:05 -0500

    warn that I need to run inside a project

--- src/leiningen/repetition_hunter.clj
+++ src/leiningen/repetition_hunter.clj
@@ -3,6 +3,7 @@
             [clojure.pprint :refer :all]
             [clojure.tools.namespace.find :refer [find-namespaces]]
             [leiningen.core.eval :refer :all]
+            [leiningen.core.main :refer :all]
             [repetition.hunter :refer :all]))
 
 (defn str-all-ns []
@@ -15,15 +16,17 @@
 
 (defn ^:no-project-needed repetition-hunter
   [project & args]
-  (let [paths (filter some? (concat
-                              (:source-paths project)
-                              [(:source-path project)]
-                              (mapcat :source-paths (get-in project [:cljsbuild :builds]))
-                              (mapcat :source-paths (get-in project [:cljx :builds]))))
-        ns-xs (find-namespaces (map io/file paths))
-        req `(do (use 'repetition.hunter)
-                 (doseq [n# '~ns-xs]
-                   (require n#)))]
-    (eval-in-project project
-                     `(hunt '~ns-xs)
-                     req)))
+  (if (empty? project)
+    (warn "lein repetition-hunter needs to run inside a project")
+    (let [paths (filter some? (concat
+                               (:source-paths project)
+                               [(:source-path project)]
+                               (mapcat :source-paths (get-in project [:cljsbuild :builds]))
+                               (mapcat :source-paths (get-in project [:cljx :builds]))))
+          ns-xs (find-namespaces (map io/file paths))
+          req `(do (use 'repetition.hunter)
+                   (doseq [n# '~ns-xs]
+                     (require n#)))]
+      (eval-in-project project
+                       `(hunt '~ns-xs)
+                       req))))
