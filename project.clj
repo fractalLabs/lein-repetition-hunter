5f28e6758bd5f6de5aa16ba64da11638be00422f
Andres Gomez Urquiza <andres@fractal.ai>
2016-05-31 22:27:45 -0500

    :url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-repetition-hunter "0.1.0-SNAPSHOT"
   :description "Find repetitions in code"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/fractalLabs/lein-repetition-hunter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.namespace "0.2.11"]
